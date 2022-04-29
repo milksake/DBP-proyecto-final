1f5101cae742af61ef78eba1666b813c6897fc3a
milksake <caceresmariana443@gmail.com>
2022-04-28 23:21:48 -0500

    login working :)

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
+from unicodedata import name
 from flask import Flask, redirect, render_template, request, url_for
-from flask_login import LoginManager, current_user, login_user, logout_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from user import users, get_user, User
 
 app = Flask(__name__)
@@ -10,7 +11,7 @@ app.config['SECRET_KEY'] = 'GRUPO8DEDESARROLLOBASADOENPLATAFORMAS'
 @login_manager.user_loader
 def load_user(user_id):
     for user in users:
-        if user.id == int(user_id):
+        if user.id == user_id:
             return user
     return None
 
@@ -26,24 +27,25 @@ def login():
         user = get_user(request.form['username'])
         if user and user.check_password(request.form['password']):
             login_user(user, remember=True)
-            return "Te acabas de loggear"
+            return render_template('index.html')
     return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return "Ya estas loggeado"
+        return redirect(url_for('index'))
     if request.method == 'POST':
         newUser = User(str(len(users)), request.form['username'], request.form['password'])
         users.append(newUser)
         login_user(newUser, remember=True)
-        return "Te acabas de loggear"
+        return redirect(url_for('index'))
     return render_template('register.html')
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return "Te desloggeaste(?)"
+    return redirect(url_for('index'))
     
 @app.route('/cart')
 def cart():
