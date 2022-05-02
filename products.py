8233cfb30239da5930baeb52de8e6e5efce496f6
Lenovo <Lenovo@DESKTOP-3UFHVN0>
2022-05-01 21:38:52 -0500

    products

--- products.py
+++ products.py
@@ -8,14 +8,17 @@ class Product():
         self.image = image
         self.description = description
 
-ejemplo = Product(1, "sample", 5, 100, "link", "sample_text")
+ejemplo1 = Product(1, "Audifonos", 5, 100, "../static/audifonos.jpg", "audifonos bonitos")
+ejemplo2 = Product(2, "Polo", 5, 100, "../static/polo.jpg", "audifonos bonitos")
+ejemplo3 = Product(3, "tazas", 5, 100, "../static/tazas.jpg", "audifonos bonitos")
+ejemplo4 = Product(4, "bolsa", 5, 100, "../static/bolso.jpg", "audifonos bonitos")
 #lista de productos
-products = [ejemplo]
+products = [ejemplo1, ejemplo2, ejemplo3, ejemplo4 ]
 
 #Búsqueda de producto por ID
 def get_product(id):
     for x in products:
         if x.id == id:
             #Retorna toooodos los datos del producto
-            return x.id, x.name, x.stars ,x.price, x.image, x.description
-    return None
+            return x
+    return None  
