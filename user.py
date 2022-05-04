f8a04d37b70cffad8a3f721c6698fcc193bcef8c
milksake <caceresmariana443@gmail.com>
2022-05-04 17:43:29 -0500

    added alerts when logging in

--- user.py
+++ user.py
@@ -2,9 +2,10 @@ from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
 class User(UserMixin):
-    def __init__(self, id, username, password, is_admin=False) -> None:
+    def __init__(self, id, username, email, password, is_admin=False) -> None:
         self.id = id
         self.username = username
+        self.email = email
         self.password = generate_password_hash(password)
         self.is_admin = is_admin
     
@@ -13,11 +14,17 @@ class User(UserMixin):
     
     def check_password(self, password):
         return check_password_hash(self.password, password)
-    
+
 users = []
 
 def get_user(username):
     for user in users:
         if user.username == username:
+            return user
+    return None
+
+def get_user_from_email(email):
+    for user in users:
+        if user.email == email:
             return user
     return None
